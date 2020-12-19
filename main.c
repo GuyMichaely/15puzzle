10c342c2b140b1fba93b424452f1b529920136a3
GuyMichaely <guymichaely@gmail.com>
2020-12-18 22:58:23 -0500

    Fixes grid again The last commit produces slightly asymmetrical grids. This one does not from my testing. I am not working on this cursed grid symmetrization algorithm anymore.

--- main.c
+++ main.c
@@ -3,6 +3,15 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+#define interactiveDebug(...) { \
+	mvprintw(0, 0, "                                                                                                                                                                            "); \
+	mvprintw(0, 0, __VA_ARGS__); \
+	if (getch() == 'q') { \
+		endwin(); \
+		exit(0); \
+	} \
+}
+	
 // d is the length of the dimension
 // margin length and grid length will be stored in *margin, *length
 int getMargin(int d){
@@ -10,58 +19,70 @@ int getMargin(int d){
 	return d / 6 + (d % 6 > modLimit);
 }
 
-void interactiveDebug(char *w, int v) {
-	mvprintw(0, 0, "                   ");
-	mvprintw(0, 0, w, v);
-	if (getch() == 'q') {
-		endwin();
-		exit(0);
-	}
-}
 
 // given the length of the dimension and number of cells to split it into
 // stores line coordinates (relative to edge of box, not edge of screen)_in lineCoords
-void getLineCoords(int length, int cells, int lineCoords[]) {
+void getCoords(int length, int cells, int lineCoords[], int cellCoords[]) {
 	// most likely won't be able to divide the grid perfectly evenly
 	// therefore spread the larger ones out throughtout the dimension
 	// symmetrically and evenly
 
+	// want to calculate seperators as though the lines don't take up space
+	// will account for the difference when storing the data
+	length -= (cells - 1);
+
 	// if there is an odd number of lines set the middle one
 	if (!(cells % 2)) {
-		lineCoords[cells / 2 - 1] = length / 2;
+		lineCoords[cells / 2 - 1] = length / 2 + (cells - 1) / 2; // (+ (cells - 1) / 2 to account for the space the lines take
 	}
 
 	// iterate through gridlines out to in
 	// set each gridline symmetrically on the left and right of the center
-	for (int i = 0; i < (cells + 1) / 2 - 1; i++) {
-		lineCoords[i] = length * (i + 1) / cells;
-		lineCoords[(cells - 2) - i] = length - lineCoords[i]; // (cells - 2) bc number of lines is cells - 1
-		// then subtract 1 again to avoid overflow; cells - 2
+	const int lastIndex = (cells + 1) / 2 - 1;
+	for (int i = 0; i < lastIndex; i++) {
+		lineCoords[i] = length * (i + 1) / cells + i;
+		lineCoords[(cells - 2) - i] = length - lineCoords[i] + ((cells - 1) - 1); // + (cells - 1) to get length back to where it was, - 1 because last line is always too short
 	}
-}
 
-// given coordinates of cell seperators
-// stores where the numbers should be placed along the dimension in cellCoords
-void getCellCoords(int firstLength, const int lineCoords[], int cellCoords[]) {
-
+	// calculate cell coordinates
+	length += (cells - 1);
+	cellCoords[0] = (lineCoords[0] - 1) / 2;
+	cellCoords[cells - 1] = length - cellCoords[0] - 1; // - 1 because 1 too far to the right otherwise
+	for (int i = 1; i < (cells + 1) / 2; i++) {
+		cellCoords[i] = (lineCoords[i] + lineCoords[i - 1]) / 2;
+		cellCoords[cells - 1 - i] = length - cellCoords[i] - 1;
+	}
 }
 
 void init(int rows, int cols) {
 	int xMargin = getMargin(COLS);
 	int xLines[cols - 1];
-	getLineCoords(COLS - 2 * xMargin, cols, xLines);
+	int xCells[cols];
+	getCoords(COLS - 2 * xMargin, cols, xLines, xCells);
 
 	int yMargin = getMargin(LINES);
 	int yLines[rows - 1];
-	getLineCoords(LINES - 2 * yMargin, rows, yLines);
+	int yCells[rows];
+	getCoords(LINES - 2 * yMargin, rows, yLines, yCells);
 
+	// print gridlines
 	for (int i = 0; i < cols - 1; i++) {
 		mvvline(yMargin, xLines[i] + xMargin, '|', LINES - 2 * yMargin);
 	}
 	for (int i = 0; i < rows - 1; i++) {
 		mvhline(yLines[i] + yMargin, xMargin, '-', COLS - 2 * xMargin);
 	}
-	
+	for (int x = 0; x < cols - 1; x++) {
+		for (int y = 0; y < rows - 1; y++) {
+			mvaddch(yLines[y] + yMargin, xLines[x] + xMargin, '+');
+		}
+	}
+	// demo print cells
+	for (int x = 0; x < cols; x++) {
+		for (int y = 0; y < rows; y++) {
+			mvprintw(yCells[y] + yMargin, xCells[x] + xMargin, "x");
+		}
+	}
 }
 
 int main(int argc, char *argv[]) {
