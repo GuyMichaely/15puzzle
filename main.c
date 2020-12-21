7b0a3f9c88da179adaad53e88808d79347ce645b
GuyMichaely <guymichaely@gmail.com>
2020-12-20 23:38:58 -0500

    Display numbers Display numbers in the grid properly centered. Also fix a bug with calculating gridlines

--- main.c
+++ main.c
@@ -19,10 +19,9 @@ int getMargin(int d){
 	return d / 6 + (d % 6 > modLimit);
 }
 
-
 // given the length of the dimension and number of cells to split it into
 // stores line coordinates (relative to edge of box, not edge of screen)_in lineCoords
-void getCoords(int length, int cells, int lineCoords[], int cellCoords[]) {
+int getCoords(int length, int cells, int lineCoords[], int cellCoords[]) {
 	// most likely won't be able to divide the grid perfectly evenly
 	// therefore spread the larger ones out throughtout the dimension
 	// symmetrically and evenly
@@ -44,21 +43,63 @@ void getCoords(int length, int cells, int lineCoords[], int cellCoords[]) {
 		lineCoords[(cells - 2) - i] = length - lineCoords[i] + ((cells - 1) - 1); // + (cells - 1) to get length back to where it was, - 1 because last line is always too short
 	}
 
+	// if there's an odd number of lines
+	// it can happen that the (n / 2 + 1)th section is too large
+	// check and account for that
+	if (!(cells % 2)) {
+		const int middle = cells / 2 - 1;
+		if (lineCoords[middle + 2] - lineCoords[middle + 1] > 1) {
+			lineCoords[middle + 1]--;
+		}
+	}
+
 	// calculate cell coordinates
 	length += (cells - 1);
-	cellCoords[0] = (lineCoords[0] - 1) / 2;
-	cellCoords[cells - 1] = length - cellCoords[0] - 1; // - 1 because 1 too far to the right otherwise
+
+	cellCoords[0] = (lineCoords[0]) / 2;
+	cellCoords[cells - 1] = (length + lineCoords[cells - 2]) / 2;
 	for (int i = 1; i < (cells + 1) / 2; i++) {
-		cellCoords[i] = (lineCoords[i] + lineCoords[i - 1]) / 2;
-		cellCoords[cells - 1 - i] = length - cellCoords[i] - 1;
+		cellCoords[cells - 1 - i] = (lineCoords[cells - 1 - i] + lineCoords[cells - 2 - i]) / 2;
+		cellCoords[i] = (lineCoords[i] + lineCoords[i - 1] + 1) / 2;
+	}
+	
+	/*
+	interactiveDebug("length: %i", length);
+	for (int i = 0; i < cells - 1; i++) {
+		interactiveDebug("lineCoords[%i]: %i", i, lineCoords[i]);
+	}
+	for (int i = 0; i < cells; i++) {
+		interactiveDebug("cellCoords[%i]: %i", i, cellCoords[i]);
+	}
+	*/
+	return length - lineCoords[cells - 2] - 1; // - 1 because the thickness of the line takes up space
+}
+
+// only for positive integers
+int intLength(int x) {
+	int length = 1;
+	for (int lt = 10; lt - 1 < x; lt = lt * 10) {
+		length++;
+	}
+	return length;
+}
+
+// when drawing numbers in grid, different numbers will need to be offset differently
+// stores offsets in offsets
+void getOffsets(const int lines, const int lastLength, const int lineCoords[], int offsets[]) {
+	offsets[lines / 2] = lastLength % 2;
+	for (int i = lines / 2 + 1; i < lines; i++) {
+		offsets[0] = !((lineCoords[i] + lineCoords[ i - 1]) % 2);
 	}
 }
 
 void init(int rows, int cols) {
-	int xMargin = getMargin(COLS);
 	int xLines[cols - 1];
 	int xCells[cols];
-	getCoords(COLS - 2 * xMargin, cols, xLines, xCells);
+	int xMargin = getMargin(COLS);
+	const int lastLength = getCoords(COLS - 2 * xMargin, cols, xLines, xCells);
+	int offsets[cols];
+	getOffsets(cols - 1, lastLength, xCells, offsets);
 
 	int yMargin = getMargin(LINES);
 	int yLines[rows - 1];
@@ -77,10 +118,20 @@ void init(int rows, int cols) {
 			mvaddch(yLines[y] + yMargin, xLines[x] + xMargin, '+');
 		}
 	}
-	// demo print cells
-	for (int x = 0; x < cols; x++) {
-		for (int y = 0; y < rows; y++) {
-			mvprintw(yCells[y] + yMargin, xCells[x] + xMargin, "x");
+
+	// draw numbers in cells
+	for (int y = 0; y < rows; y++) {
+		for (int x = 0; x < (cols + 1) / 2; x++) {
+			const int yCoord = yCells[y] + yMargin;
+			int xCoord = xCells[x] + xMargin;
+			xCoord -= intLength(cols * y + x) / 2;
+			mvprintw(yCoord, xCoord, "%i", cols * y + x);
+		}
+		for (int x = (cols + 1) / 2; x < cols; x++) {
+			const int yCoord = yCells[y] + yMargin;
+			int xCoord = xCells[x] + xMargin;
+			xCoord -= (intLength(cols * y + x) - 1) / 2;
+			mvprintw(yCoord, xCoord, "%i", cols * y + x);
 		}
 	}
 }
